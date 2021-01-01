e0d67601e9e264bc135fcef3a18749b6918041af
Chema <chema>
2021-01-01 09:54:03 +0100

    funcion de dolar echa y funcionando bien

--- srcs/ft_dolar.c
+++ srcs/ft_dolar.c
@@ -6,7 +6,7 @@
 /*   By: jalcayne <jalcayne@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/31 10:23:28 by jalcayne          #+#    #+#             */
-/*   Updated: 2020/12/31 10:39:10 by jalcayne         ###   ########.fr       */
+/*   Updated: 2021/01/01 09:52:47 by jalcayne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,26 @@
 int     ft_dolar(char *comand, t_mini *mini)
 {
 	int i;
-
+	int flag;
+	
 	i = 0;
+	flag = 0;
 	while ((ft_isalpha(comand[i] == 0)) && comand[i])
 		i++;
-	if (ft_strncmp(&comand[i], "echo ", 5))
+	if ((ft_strncmp(&comand[i], "echo ", 5) == 0))
 	{
-		while (ft_isalpha(comand[i] != '$') && comand[i])
+		while ((comand[i] != '$') && comand[i])
+		{
+			if (comand[i] == '-' && comand[i + 1] == 'n')
+				flag = 1;
 			i++;
+		}
 		if (comand[i] && comand[i + 1] == '?')
 		{
-			ft_printf("%d", mini->dolar);
+			if (flag)
+				ft_printf("%d", mini->dolar);
+			else
+				ft_printf("%d\n", mini->dolar);
 			return (1);
 		}
 	}
